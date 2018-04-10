8cd4d72b2c0a115e90a426a4f2b60de55a8a67c3
zhaoliu09 <liulianjushi@126.com>
2018-04-10 14:54:28 +0800

    add swagger

--- src/main/java/com/atits/config/SwaggerConfig.java
+++ src/main/java/com/atits/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package com.atits.config;
 
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
