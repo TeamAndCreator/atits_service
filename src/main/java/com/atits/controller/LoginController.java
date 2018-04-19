98756994a5c9db02fc1d7eada0fcd4efc9ee1a6e
lh <1532589995@qq.com>
2018-04-19 21:12:48 +0800

    register_controller

--- src/main/java/com/atits/controller/LoginController.java
+++ src/main/java/com/atits/controller/LoginController.java
@@ -1,10 +1,5 @@
 package com.atits.controller;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-
 import com.atits.entity.Station;
 import com.atits.entity.User;
 import com.atits.entity.Msg;
@@ -14,19 +9,12 @@ import io.swagger.annotations.*;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
-import org.hibernate.engine.spi.SelfDirtinessTracker;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.DigestUtils;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
-import sun.plugin.com.Utils;
 
 import javax.annotation.Resource;
-import java.net.BindException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @Api(value = "product", description = "登录接口")
@@ -81,45 +69,6 @@ public class LoginController {
         return Msg.success();
     }
 
-
-    @ApiOperation("用户注册")
-//        @ApiImplicitParams({
-//            @ApiImplicitParam(name = "userName", value = "用户名", required = true),
-//            @ApiImplicitParam(name = "password", value = "用户密码", required = true)
-////            @ApiImplicitParam(name = "system.id",value = "系统ID",required = true)
-//    }
-//    )
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
-    @ResponseBody
-    public Msg register( User user){
-        //用户名唯一
-        try{
-            User findByUserName = this.userService.findByUserName(user.getUserName());
-            if (findByUserName != null){
-                System.out.println("用户名已存在！请换个账号");
-                return Msg.fail();
-            }
-            user.setState(2);//未激活
-            try{
-                if (user.getPassword() !=null &&  !"".equals(user.getPassword())){
-                    String userPwd = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
-                    user.setPassword(userPwd);
-                }
-                Date time = new Date();
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                user.setCreateTime(dateFormat.format(time));
-                this.userService.save(user);
-            }catch (Exception e){
-                System.out.println("保存用户信息异常！");
-                e.printStackTrace();
-                return Msg.fail();
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-            return Msg.fail();
-        }
-        return Msg.success();
-    }
 
 
     @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
---
+++ src/main/java/com/atits/controller/RegisterController.java
@@ -0,0 +1,72 @@
+package com.atits.controller;
+
+import com.atits.entity.Msg;
+import com.atits.entity.User;
+import com.atits.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@Api(value = "product", description = "注册接口")
+public class RegisterController {
+
+    @Resource
+    private UserService userService;
+
+    @ApiOperation("用户注册")
+    @ApiResponses({
+            @ApiResponse(code= 201 ,message = "已创建"),
+            @ApiResponse(code = 400, message = "请求参数填写错误 "),
+            @ApiResponse(code= 401 ,message = "访问被拒绝"),
+            @ApiResponse(code= 403 ,message = "禁止访问"),
+            @ApiResponse(code= 404 ,message = "请求路径没有或页面跳转路径错误")
+    })
+//        @ApiImplicitParams({
+//            @ApiImplicitParam(name = "userName", value = "用户名", required = true),
+//            @ApiImplicitParam(name = "password", value = "用户密码", required = true)
+////          @ApiImplicitParam(name = "system.id",value = "系统ID",required = true)
+//    }
+//    )
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    @ResponseBody
+    public Msg register(User user){
+        //用户名唯一
+        try{
+            User findByUserName = this.userService.findByUserName(user.getUserName());
+            if (findByUserName != null){
+                System.out.println("用户名已存在！请换个账号");
+                return Msg.fail();
+            }
+            user.setState(2);//未激活
+            try{
+                if (user.getPassword() !=null &&  !"".equals(user.getPassword())){
+                    String userPwd = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+                    user.setPassword(userPwd);
+                }
+                Date time = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                user.setCreateTime(dateFormat.format(time));
+                this.userService.save(user);
+            }catch (Exception e){
+                System.out.println("保存用户信息异常！");
+                e.printStackTrace();
+                return Msg.fail();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return Msg.fail();
+        }
+        return Msg.success();
+    }
+}
