2bf3347b1c3962c47cebff14d534a5e3128f42fd
zhaoliu09 <liulianjushi@126.com>
2018-04-27 23:34:12 +0800

    add role management interface function

---
+++ src/main/java/com/atits/service/RoleService.java
@@ -0,0 +1,25 @@
+package com.atits.service;
+
+
+import com.atits.dao.RoleDao;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Transactional
+@Service
+public class RoleService {
+
+    @Resource
+    private RoleDao roleDao;
+
+    /**
+     * 查询所有角色
+     * @return List
+     */
+    public List findAll(){
+        return roleDao.findAll();
+    }
+}
